99ba1c1d3f3daccd255b9f0e265d049191522cc6
Jan Romaniak <romaniakjan@gmail.com>
2017-08-23 10:36:39 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express')
 const app = express()
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
+isRecording=false;
+app.get('/isRecording/', function (req, res) {
+  res.send(isRecording)
 })
-
+app.get('/changeState', function (req, res) {
+  isRecording=!isRecording
+  res.send(isRecording)
+})
 app.listen(process.env.PORT, function () {
   console.log('Example app listening on port'+process.env.PORT+'!')
 })
